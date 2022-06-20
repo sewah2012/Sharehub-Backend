793a1a592a36ecf8d8afc161afa7116b978cadb3
Emmanuel Sahr Sewah <e.sewah@peaqock.com>
2022-06-20 20:06:25 +0100

    add procfile and other heroku stuffs

--- src/main/java/com/luslusdawmpfe/PFEBackent/PfeBackentApplication.java
+++ src/main/java/com/luslusdawmpfe/PFEBackent/PfeBackentApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.yml")
 public class PfeBackentApplication {
 @Autowired
 private AppUserRepo appUserRepo;
