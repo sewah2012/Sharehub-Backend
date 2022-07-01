4e11dd201238119b0b6dd045f235ed2c5d914b37
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2022-07-01 12:32:26 +0100

    update: return updated experience instead of string

--- src/main/java/com/luslusdawmpfe/PFEBackent/services/ExperienceService.java
+++ src/main/java/com/luslusdawmpfe/PFEBackent/services/ExperienceService.java
@@ -4,9 +4,7 @@ import com.luslusdawmpfe.PFEBackent.dtos.AddExperienceDto;
 import com.luslusdawmpfe.PFEBackent.dtos.ExperienceDto;
 import com.luslusdawmpfe.PFEBackent.dtos.UpdateExperienceRequest;
 import com.luslusdawmpfe.PFEBackent.entities.AppUser;
-import com.luslusdawmpfe.PFEBackent.entities.Experience;
 import com.luslusdawmpfe.PFEBackent.exceptions.EntityNotFoundException;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,7 +14,7 @@ public interface ExperienceService {
     ExperienceDto shareExperience(MultipartFile[] files, AddExperienceDto experience, AppUser user);
     ResponseEntity<List<ExperienceDto>> listExperiences(int pageNumber, int pageSize, String softBy);
     ExperienceDto getOneExperience(String experienceId) throws EntityNotFoundException;
-    String updateExperience(UpdateExperienceRequest request) throws EntityNotFoundException;
+    ExperienceDto updateExperience(UpdateExperienceRequest request) throws EntityNotFoundException;
     String deleteExperience(String experience) throws EntityNotFoundException;
 
     ResponseEntity<List<ExperienceDto>> listPopularExperiences();
